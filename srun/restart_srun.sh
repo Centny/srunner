01f615bcedf1de1e449cbcd99a131e53d60e47c5
Centny <centny@gmail.com>
2016-03-08 13:26:42 +0800

    adding path .

--- srun/restart_srun.sh
+++ srun/restart_srun.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd `dirname ${0}`
+export PATH=`pwd`:`dirname ${0}`:$PATH
 srun -c http://127.0.0.1:3010/_exit_
--- srun/stop_srun.sh
+++ srun/stop_srun.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd `dirname ${0}`
+export PATH=`pwd`:`dirname ${0}`:$PATH
 touch .sr_done
 srun -c http://127.0.0.1:3010/_exit_
